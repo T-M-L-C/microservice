93343398bbb7b7b9c67f049c7a0f949a728485bb
liuchao30 <245715460@qq.com>
2017-10-06 23:15:24 +0800

    微服务和SpringCloud

---
+++ ui/src/main/java/com/itmyhome/UiApplication.java
@@ -0,0 +1,16 @@
+package com.itmyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class UiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+}
